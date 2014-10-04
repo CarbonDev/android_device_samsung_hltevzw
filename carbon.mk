aa69436e78cbf025ff1c62393bb3f1252da0bdb2
mattmanwrx <mbloomfield87@gmail.com>
2014-10-04 18:12:13 +0100

    hltevzw: Carbonize
    Change-Id: I7550d25cd41352c7708d056723cb04e8bd0b052a
    Signed-off-by: mattmanwrx <mbloomfield87@gmail.com>

---
+++ carbon.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, device/samsung/hltevzw/full_hltevzw.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
+
+PRODUCT_DEVICE := hltevzw
+PRODUCT_NAME := carbon_hltevzw
